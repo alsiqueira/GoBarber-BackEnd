04d9cc6bcf516b19069bb480ab4bd51f42ec7ef0
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-01-12 20:00:48 -0300

    Create List Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,11 +4,15 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
-
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
   const findAppointmentsInSameDate = appointmentsRepository.findByDate(
     parsedDate,
   );
@@ -17,9 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
-
   const appointment = appointmentsRepository.create(provider, parsedDate);
-
   return response.json(appointment);
 });
 
