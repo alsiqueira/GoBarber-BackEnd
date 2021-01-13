069f6c18c17687008572a374f211b0b3664fd53c
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-01-12 22:56:38 -0300

    Create/Config module appointments

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
