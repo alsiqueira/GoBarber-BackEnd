fe1eabf029b7f3f9350398e38f7b6d7f96e2ad21
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-01-13 22:21:23 -0300

    Model e Migration Users

--- src/database/migrations/1610586350475-CreateUsers.ts
+++ src/database/migrations/1610586350475-CreateUsers.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1610501432236
-  implements MigrationInterface {
+export default class CreateUsers1610586350475 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,14 +14,17 @@ export default class CreateAppointments1610501432236
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -41,6 +42,6 @@ export default class CreateAppointments1610501432236
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
