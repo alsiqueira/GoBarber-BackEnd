d5e4bb127d8e7f78594f5fdf8fb9e3e1de3a9c88
Alexandre Luis Siqueira <alexandre1760@hotmail.com>
2021-01-15 20:49:04 -0300

    updating avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,15 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tempFolder,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
-    filename(request, file, callback) {
+    destination: tempFolder,
+    filename: (request, file, callback) => {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
-
       return callback(null, fileName);
     },
   }),
